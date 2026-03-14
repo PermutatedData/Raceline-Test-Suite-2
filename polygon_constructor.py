f096c2945e12a146738e0c35bb9d24a648e9ec34
Richard Zhang <edwardisawsome419@gmail.com>
2026-03-14 14:14:20 -0400

    Combines left and right sorted cones into full polygon

--- polygon_constructor.py
+++ polygon_constructor.py
@@ -3,8 +3,6 @@ import math
 
 TOLERANCE = 1E-5
 
-# TODO: projection sorting, something more advanced if necessary. Centroid angle sorting likely not good enough for FSAE?
-
 def score_cone(current, heading, cone, max_dist, weight_angle, min_spacing, max_spacing, max_search_angle_dot) -> float: 
     """
     Returns weighted sum between two positions factoring change in heading and spacing
@@ -40,13 +38,14 @@ def score_cone(current, heading, cone, max_dist, weight_angle, min_spacing, max_
     
     return dist / max_dist + weight_angle * (1 - dot)
 
+
 # Typical values
 # weight_angle / weight_dist = 0.67-ish
 # weight_future_look = 0.4–0.7
 
 # In reality, car pos should be 0
 # In particularly bad cases, ordering will fail rather than try a bs solution
-# TODO: set initial value of future_score. Must be high but not to the point of overflow
+# TODO: ensure initial set value of future_score is not cooked
 
 def order_boundary_weighted(cones: np.ndarray, car_pos, car_heading=0, weight_angle=0.66, weight_future_look=0.5, min_spacing=0.5, max_spacing=6, max_search_angle=70) -> np.ndarray:
     """
@@ -88,7 +87,7 @@ def order_boundary_weighted(cones: np.ndarray, car_pos, car_heading=0, weight_an
     direction_normalized = direction / np.linalg.norm(direction)
 
     while len(visited) < n:
-        print(cones[current], len(visited))
+        # print(cones[current], len(visited))
 
         best_idx = -1
         best_score = np.inf
@@ -105,7 +104,7 @@ def order_boundary_weighted(cones: np.ndarray, car_pos, car_heading=0, weight_an
 
             score = score_cone(cones[current], direction_normalized, cones[idx], max_dist, weight_angle=weight_angle, min_spacing=min_spacing, max_spacing=max_spacing, max_search_angle_dot=cos_limit)
             
-            print(score)
+            # print(score)
             
             # Should ensure this is greater than what's feasible
             future_score = 1 + weight_angle + 1
@@ -130,7 +129,7 @@ def order_boundary_weighted(cones: np.ndarray, car_pos, car_heading=0, weight_an
                     best_idx = idx
 
         if best_idx == -1:
-            print('break 2')
+            print("No new cones within bounds. Cone sorting stopped early")
             break
 
         ordered.append(cones[best_idx])
@@ -142,3 +141,49 @@ def order_boundary_weighted(cones: np.ndarray, car_pos, car_heading=0, weight_an
         current = best_idx
 
     return np.array(ordered)
+
+
+def get_good_polygon(left_cones: np.ndarray, right_cones: np.ndarray) -> np.ndarray:
+    """
+    Using the shorter of the two cone lists, finds the closest point of the other list and connects to it to create vertices of a CCW polygon
+    Assumes cones are sorted
+
+    Args:
+        left_cones (np.ndarray): _description_
+        right_cones (np.ndarray): _description_
+    """
+    
+    last_left_cone = left_cones[-1]
+    dists = np.linalg.norm(right_cones - last_left_cone, axis=1)
+    index_right = np.argmin(dists)
+    
+    last_right_cone = right_cones[-1]
+    dists2 = np.linalg.norm(left_cones - last_right_cone, axis=1)
+    index_left = np.argmin(dists2)
+        
+    min_on_left = min(dists[index_right], dists2[index_left]) == dists[index_left]
+    min_dist = 0
+    
+    if min_on_left:
+        min_dist = dists[index_left]
+
+        right_cones = right_cones[:index_right + 1]
+        
+    else:
+        min_dist = dists2[index_right]
+        
+        left_cones = left_cones[:index_left + 1]
+        
+    # TODO: separation check with min_dist
+    
+    return np.vstack((right_cones, left_cones[::-1]))
+
+
+def polygon_pipeline(left_cones: np.ndarray, right_cones: np.ndarray, car_pos, car_heading=0, weight_angle=0.66, weight_future_look=0.5, min_spacing=0.5, max_spacing=6, max_search_angle=70) -> np.ndarray:
+    left_cones_ordered = left_cones
+    right_cones_ordered = right_cones
+    
+    if len(left_cones_ordered) < 2 and len(right_cones_ordered) < 2:
+        raise ValueError("Too few cones")
+    
+    return get_good_polygon(left_cones_ordered, right_cones_ordered)
